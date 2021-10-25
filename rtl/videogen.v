6cd0d33860ac951c604082b3218fecb62f33c8de
Jeff Schoch <jeffreydschoch@gmail.com>
2021-10-25 18:13:14 -0400

    convert image to color palette and array of index per pixel, lookup index, then look up color from palette

--- rtl/videogen.v
+++ rtl/videogen.v
@@ -58,15 +58,18 @@ reg [9:0] v_cnt; //max. 1024
 parameter IMAGE_SIZE_X = 10'd180;
 parameter IMAGE_SIZE_Y = 10'd120;
 
-// Assumes 24bit color - each pixel requires 3 bytes (RGB 8 bits each, no alpha)
-parameter IMAGE_MEMORY_SIZE = IMAGE_SIZE_X * IMAGE_SIZE_Y * 3;
+// Each pixel cooresponds to a color palette index in the text file
+parameter IMAGE_MEMORY_SIZE = IMAGE_SIZE_X * IMAGE_SIZE_Y;
 
-// Hex array image memory declaration
-reg [7:0] imageHexArray24bit[0:IMAGE_MEMORY_SIZE-1];
+// Indices array memory declaration, 3 bits per pixel (index maximum of 5, only needs 3 bits)
+reg [2:0] paletteIndexArray[0:IMAGE_MEMORY_SIZE-1];
+// Color palette has six 24-bit colors in it (eighteen 8-bit components)
+reg [7:0] colorPaletteArray[0:17];
 
 initial begin
-    // read the image file into memory (file is a 24bit image converted to hex array)
-    $readmemh("startup_image_180x120.hex", imageHexArray24bit);
+    // read the indices file and the color palette into memory
+    $readmemh("colorIndices.txt", paletteIndexArray);
+    $readmemh("colorPalette.txt", colorPaletteArray);
 end
 
 //HSYNC gen (negative polarity)
@@ -142,17 +145,17 @@ begin
                 end
             endcase
         end else begin
-            // Sample RGB values from the hex array based on current position.
+            // Sample an index from the index array based on current position, use it to look up palette RGB
             // xpos and ypos divided by 4 to scale from 720x480 to 180x120.
             // If I were more confident in verilog syntax I would make the code more readable.
             // This formula:
             // 1. Take input between (0,0) and (719,479), and divide by 4 to remap to the image size
-            // 2. Multiply by 3 because each pixel has 3 array entries (RGB)
-            // 3. Multiply Y by the image width to convert the 2D coordinate into the 1D array index
+            // 2. Multiply Y by the image width to convert the 2D coordinate into the 1D array index
+            // 3. Multiply index by 3 to make index land on the R component of each palette color
             // 4. Add an offset of 1 and 2 to get the G and B components
-            R_out <= imageHexArray24bit[((ypos>>2) * 3 * IMAGE_SIZE_X) + ((xpos>>2) * 3) + 0];
-            G_out <= imageHexArray24bit[((ypos>>2) * 3 * IMAGE_SIZE_X) + ((xpos>>2) * 3) + 1];
-            B_out <= imageHexArray24bit[((ypos>>2) * 3 * IMAGE_SIZE_X) + ((xpos>>2) * 3) + 2];
+            R_out <= colorPaletteArray[(paletteIndexArray[((ypos>>2) * IMAGE_SIZE_X) + (xpos>>2)] * 3) + 0]
+            G_out <= colorPaletteArray[(paletteIndexArray[((ypos>>2) * IMAGE_SIZE_X) + (xpos>>2)] * 3) + 1];
+            B_out <= colorPaletteArray[(paletteIndexArray[((ypos>>2) * IMAGE_SIZE_X) + (xpos>>2)] * 3) + 2];
         end
         DE_out <= (h_cnt >= X_START && h_cnt < X_START + H_ACTIVE && v_cnt >= Y_START && v_cnt < Y_START + V_ACTIVE);
     end
